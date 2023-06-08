f66c02668a3bf3ddc634e58141c36141f8512106
Oscilous <artis.fils@gmail.com>
2023-06-08 19:55:56 +0200

    Part two completed

--- calorie-counting/src/main.rs
+++ calorie-counting/src/main.rs
@@ -3,8 +3,9 @@ use std::fs;
 fn main() {
     let contents: String =
         fs::read_to_string("src/test.txt").expect("Should have been able to read the file");
-    let elves: Vec<i32> = split_string(contents);
-    find_max_calories(elves);
+    let mut elves: Vec<i32> = split_string(contents);
+    find_max_calories(&elves);
+    find_top3_calories(&mut elves);
 }
 
 fn split_string(s: String) -> Vec<i32> {
@@ -15,7 +16,7 @@ fn split_string(s: String) -> Vec<i32> {
         let items: std::str::Lines = elf_string.lines();
         let mut total_calories: i32 = 0;
         for item in items {
-            let calories = item.parse::<i32>().expect("Cannot convert to i32");
+            let calories: i32 = item.parse::<i32>().expect("Cannot convert to i32");
             total_calories += calories;
         }
         split_list.push(total_calories);
@@ -23,7 +24,7 @@ fn split_string(s: String) -> Vec<i32> {
     split_list
 }
 
-fn find_max_calories(elves: Vec<i32>) {
+fn find_max_calories(elves: &Vec<i32>) {
     let most_calories: (usize, &i32) = elves
         .iter()
         .enumerate()
@@ -33,5 +34,13 @@ fn find_max_calories(elves: Vec<i32>) {
         "Elf number - {}, is carrying the most calories - {}",
         most_calories.0 + 1,
         most_calories.1
+    );
+}
+
+fn find_top3_calories(elves: &mut Vec<i32>) {
+    elves.sort_by(|x, y| y.cmp(x));
+    println!(
+        "The sum of the top 3 are {}",
+        elves[0] + elves[1] + elves[2]
     );
 }
